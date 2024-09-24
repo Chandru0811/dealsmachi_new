7b5ce67c0fdcd3349b20ba45c0480578da8cdd34
Rohini0410 <rohinidevi.cloudecs@gmail.com>
2024-09-24 18:11:22 +0530

    .

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -349,12 +349,12 @@ document.getElementById('applyButton').addEventListener('click', function () {
 });
 
 // <!-- Book Mark Icon -->
-  function toggleBookmark(element) {
-      if (element.classList.contains('fa-regular')) {
-          element.classList.remove('fa-regular');
-          element.classList.add('fa-solid');
-      } else {
-          element.classList.remove('fa-solid');
-          element.classList.add('fa-regular');
-      }
-  }
+function toggleBookmark(element) {
+    if (element.classList.contains('fa-regular')) {
+        element.classList.remove('fa-regular');
+        element.classList.add('fa-solid');
+    } else {
+        element.classList.remove('fa-solid');
+        element.classList.add('fa-regular');
+    }
+}
