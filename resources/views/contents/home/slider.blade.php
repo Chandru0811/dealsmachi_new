8540e46014eec033d463ed2426a2c7b2d2f3015a
manojecs2020 <manoj@ecscloudinfotech.com>
2024-10-21 17:49:29 +0530

    banner added

--- resources/views/contents/home/slider.blade.php
+++ resources/views/contents/home/slider.blade.php
@@ -2,22 +2,29 @@
     <div class="row">
         <div class="col-md-12 col-lg-9 col-12 mb-3">
             <div class="owl-carousel carousel_slider owl-theme">
-
+                <div class="item">
+                    <img src="{{ asset('assets/images/home/banner1.webp') }}" alt="slider_image1"
+                        class="img-fluid p-2 rounded-5 banner_image" />
+                </div>
                 <div class="item">
                     <img src="{{ asset('assets/images/home/banner2.webp') }}" alt="slider_image2"
                         class="img-fluid p-2 rounded-5 banner_image" />
                 </div>
+
                 <div class="item">
                     <img src="{{ asset('assets/images/home/banner3.webp') }}" alt="slider_image3"
                         class="img-fluid p-2 rounded-5 banner_image" />
                 </div>
-
                 <div class="item">
-                    <img src="{{ asset('assets/images/home/banner5.webp') }}" alt="slider_image4"
+                    <img src="{{ asset('assets/images/home/banner4.webp') }}" alt="slider_image4"
                         class="img-fluid p-2 rounded-5 banner_image" />
                 </div>
                 <div class="item">
-                    <img src="{{ asset('assets/images/home/banner7.webp') }}" alt="slider_image4"
+                    <img src="{{ asset('assets/images/home/banner5.webp') }}" alt="slider_image5"
+                        class="img-fluid p-2 rounded-5 banner_image" />
+                </div>
+                <div class="item">
+                    <img src="{{ asset('assets/images/home/banner6.webp') }}" alt="slider_image6"
                         class="img-fluid p-2 rounded-5 banner_image" />
                 </div>
             </div>
