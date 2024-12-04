88df1bdd9e78fe163dd697783b737169747389e9
Rohini0410 <rohinidevi.cloudecs@gmail.com>
2024-12-04 17:56:28 +0530

    updated

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -78,7 +78,6 @@ class CheckoutController extends Controller
             'state' => $request->input('state'),
             'zipCode' => $request->input('zipCode'),
         ];
-        $mobile = $request->input('mobile');
         $user_id = Auth::check() ? Auth::id() : null;
         $product = Product::with(['shop'])->where('id', $request->input('product_id'))->where('active', 1)->first();
         $latestOrder = Order::orderBy('id', 'desc')->first();
@@ -92,7 +91,7 @@ class CheckoutController extends Controller
             'first_name'       => $request->input('first_name'),
             'last_name'        => $request->input('last_name'),
             'email'            => $request->input('email'),
-            'mobile'           => $mobile,
+            'mobile'           => $request->input('mobile'),
             'order_type'       => $request->input('order_type'),
             'status'           => 1, //created
             'notes'            => $request->input('notes') ?? null,
