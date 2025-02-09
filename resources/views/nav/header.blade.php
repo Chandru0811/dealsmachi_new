0c78ce55046a8c5334aa50898d6d84d58833eef4
Rohini0410 <rohinidevi.cloudecs@gmail.com>
2025-02-09 16:16:16 +0530

    cart dropdown integration completed

--- resources/views/nav/header.blade.php
+++ resources/views/nav/header.blade.php
@@ -180,17 +180,40 @@
                             style="left: 0; transform: translate(-85%, 0);">
                             <div class="child">
                                 <p class="text_size" style="color: #cbcbcb">Recently Added Products</p>
+                                @if(!$cartItems)
+                                <div class="text-center">
+                                    <img src="{{ asset('assets/images/home/empty_cart.webp') }}" alt="Empty Cart"
+                                        class="img-fluid" width="75">
+                                    <p class="text_size" style="color: #cbcbcb">Your cart is empty</p>
+                                </div>
+                                @endif
                                 @if($cartItems)
-                                @foreach($cartItems as $item)
+                                @foreach($cartItems->take(6) as $item)
                                 <div class="d-flex">
-                                    <img src="{{ asset('assets/images/home/card_image_2.webp') }}"
-                                        alt="House Moving" class="img-fluid dropdown_img">
+                                    @php
+                                    $image = isset($item->product->productMedia)
+                                    ? $item->product->productMedia
+                                    ->where('order', 1)
+                                    ->where('type', 'image')
+                                    ->first() : null;
+                                    @endphp
+                                    <img src="{{ $image ? asset($image->resize_path) : asset('assets/images/home/noImage.webp') }}"
+                                        class="img-fluid dropdown_img" alt="{{ $item->product->name }}" />
                                     <div class="text-start">
-                                        <p class="text-start px-1 text-wrap m-0 p-0" style="font-size: 12px; white-space: normal;">Shirt</p>
-                                        <p class="px-1 text_size" style="color: #ff0060">₹70</p>
+                                        <p class="text-start px-1 text-wrap m-0 p-0" style="font-size: 12px; white-space: normal;">
+                                            {{ \Illuminate\Support\Str::limit($item->product->name, 20) }}
+                                        </p>
+                                        <p class="px-1 text_size" style="color: #ff0060">
+                                            ₹ {{ number_format($item->discount, 0) }}
+                                        </p>
                                     </div>
                                 </div>
                                 @endforeach
+                                @if($cartItems->count() > 6)
+                                <div class="text-end mb-2">
+                                    <a style="font-size: 13px" href="{{ route('cart.index') }}">View All</a>
+                                </div>
+                                @endif
                                 @endif
                                 <div class="dropdown_cart_view d-flex justify-content-end">
                                     <a href="{{ route('cart.index') }}"
